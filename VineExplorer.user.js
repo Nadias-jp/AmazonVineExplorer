dec9c397db072ba800717f922961febe5f3a9dca
Nadias-jp <nadias.jp@icloud.com>
2024-06-17 23:31:13 +0900

    Update VineExplorer.user.js
    Support Amazon.co.jp

--- VineExplorer.user.js
+++ VineExplorer.user.js
@@ -9,6 +9,7 @@
 // @match        *://www.amazon.de/*
 // @match        *://www.amazon.com/*
 // @match        *://www.amazon.co.uk/*
+// @match        *://www.amazon.co.jp/*
 // @license      MIT
 // @icon         https://raw.githubusercontent.com/Amazon-Vine-Explorer/AmazonVineExplorer/main/vine_logo.png
 // @run-at       document-start
