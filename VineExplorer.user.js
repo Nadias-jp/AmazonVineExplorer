557553f6f5f75f1c9042100045523d84e2da408c
Nadias-jp <nadias.jp@icloud.com>
2024-12-13 00:47:59 +0900

    Fix typo

--- VineExplorer.user.js
+++ VineExplorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Vine Explorer
 // @namespace    http://tampermonkey.net/
-// @version      0.11.03.jp
+// @version      0.11.04.jp
 // @updateURL    https://raw.githubusercontent.com/Nadias-jp/AmazonVineExplorer/main/VineExplorer.user.js
 // @downloadURL  https://raw.githubusercontent.com/Nadias-jp/AmazonVineExplorer/main/VineExplorer.user.js
 // @description  Better View, Search and Explore for Amazon Vine Products - Vine Voices Edition
@@ -3167,7 +3167,6 @@ function init(hasTiles) {
   _searchbarContainer.appendChild(
     createNavButton(
       translation[langCode]["ave-btn-favorites"],
-      "Favoriten",
       "",
       SETTINGS.BtnColorFavorites,
       () => {
